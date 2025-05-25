4c67ea78f3e4faad8685077c9a4da93294d589fe
Rasul Imyarzhanov <Rassul.Imyarzhanov@raimbek.com>
2025-05-25 19:28:17 +0500

    Add tasks model and display on admin page

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from tasks.models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'completed', 'owner', 'created_at', 'updated_at')
+    list_filter = ('completed', 'owner', 'created_at')
+    search_fields = ('title', 'description', 'owner__username')
+    ordering = ('-created_at',)
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from core.models import BaseModel
+
+
+class Task(BaseModel):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='tasks')
+    title = models.CharField(_("название"), max_length=200)
+    description = models.TextField( _("описание"), blank=True, null=True)
+    completed = models.BooleanField(_("выполнено"), default=False)
+
+    class Meta:
+        ordering = ["-created_at"]
+        verbose_name = _("задача")
+        verbose_name_plural = _("задачи")
+
+    def __str__(self):
+        return self.title
